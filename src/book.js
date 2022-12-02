9464f2e4a0385bf219cfa439b2b15bb19864977c
Barbie Ordonez <ordonezbarbie@gmail.com>
2022-12-02 12:02:39 -0700

    Add functions to book file

--- src/book.js
+++ src/book.js
@@ -1,12 +1,49 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`
 }
+
+function buildMainCharacter(name, age, pronouns) {
+var person = {
+  name,
+  age,
+  pronouns
+}
+  return person
+}
+
+function saveReview(newReviews, existingReviews){
+  if (existingReviews.includes(newReviews)) {
+    return 
+  } else
+  return existingReviews.push(newReviews)
+}
+
+function calculatePageCount(title) {
+  
+  return title.length * 20
+}
+
+function writeBook(title, mainCharacter, genre){
+return  book = {
+  title: title,
+  mainCharacter: mainCharacter,
+  pageCount: calculatePageCount(title),
+  genre: genre
+}
+}
+
+function editBook(book){
+   var total = book.pageCount * .75
+  book.pageCount = total
+   return total
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
