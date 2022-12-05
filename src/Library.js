398d2f685936e06d96e806076436cf41b72df4e4
Barbie Ordonez <ordonezbarbie@gmail.com>
2022-12-04 21:22:26 -0700

    Add library answers

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,31 @@
 
 
+function createLibrary (libraryName){
+  return {
+    name: libraryName, 
+    shelves: {fantasy: [], fiction: [], nonFiction: []} 
+  }
+}
+
+
+function addBook(library, book){
+  library.shelves[book.genre].push(book)
+}
+
+
+function checkoutBook (library, bookName, genre) {
+ for (var i = 0; i < library.shelves[genre].length ; i++) {
+ if (bookName === library.shelves[genre][i].title) {
+ library.shelves[genre].splice(i, 1)
+ return `You have now checked out ${bookName} from the ${library.name}`} 
+ }
+ return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
+ }
+
+
+
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
